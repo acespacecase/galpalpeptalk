53bb8b732173d43265ef275cf809f8f574875626
Stacy Harrison <stacy.e.harrison@gmail.com>
2017-03-10 19:02:41 -0500

    finished js functionality

--- js/custom.js
+++ js/custom.js
@@ -15,7 +15,7 @@ newLyric.addEventListener("click", function() {
 });
 
 function renderHTML(data) {
-  var pickSong = Math.floor((Math.random() * data.length));
+  var pickSong = Math.floor(Math.random() * data.length);
 
   // insert song name and artist into the dom
   artistContainer.innerHTML = "\"" + data[pickSong].songName + "\"" + " by " + data[pickSong].artist;
@@ -23,15 +23,8 @@ function renderHTML(data) {
   // update listen link
   listenLink.innerHTML = '<a class="btn btn-outline btn-row" id="listenLink" type="button" target="_blank" href="' + data[pickSong].link + '">listen</a>';
 
-
-
-  //
-  //
-  //
-  // var htmlString = "";
-  //
-  // htmlString += "<p>" + data[0].songName + "</p>";
-  //
-  // quoteContainer.insertAdjacentHTML('beforeend', htmlString);
+  // find random lyric within specific song object; insert it into the dom
+  var pickLyric = Math.floor(Math.random() * data[pickSong].quotes.length);
+  quoteContainer.innerHTML = data[pickSong].quotes[pickLyric];
 
 }
